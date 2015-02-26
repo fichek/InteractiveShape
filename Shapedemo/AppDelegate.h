c761b80890d87c5bd77cb6f38bf32a1b267ac711
Filip Radelic <filip@filip.local>
2015-02-26 21:39:18 +0100

    Initial Commit

---
+++ Shapedemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shapedemo
+//
+//  Created by Filip Radelic on 26/02/15.
+//  Copyright (c) 2015 Filip Radelic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shapedemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shapedemo
+//
+//  Created by Filip Radelic on 26/02/15.
+//  Copyright (c) 2015 Filip Radelic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
